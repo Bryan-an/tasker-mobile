cfba3e911413605a2a6c2c6d799437ce249f299f
Bryan-an <bryanandagoya@gmail.com>
2023-04-20 16:13:47 -0500

    task form screen done

--- lib/src/features/tasks/presentation/screens/task_form/cubit/task_form_screen_cubit.dart
+++ lib/src/features/tasks/presentation/screens/task_form/cubit/task_form_screen_cubit.dart
@@ -1,7 +1,10 @@
-import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tasker_mobile/src/constants/export.dart';
 
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'task_form_screen_cubit.freezed.dart';
 part 'task_form_screen_state.dart';
 
 class TaskFormScreenCubit extends Cubit<TaskFormScreenState> {
